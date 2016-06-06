b254e84c016ab835c90d691aaa02b248ec3783ab
Zack Nagaich <zacknagaich@gmail.com>
2016-06-06 10:56:16 -0400

    Added hash lookup support

--- white_walker.py
+++ white_walker.py
@@ -2,12 +2,40 @@
 import socket
 import re
 import requests
+from pythreatgrid.threatgrid import get_analysis, search_samples
+import json
+from pprint import pprint
 
 class WhiteWalker():
 
 
-	def __init__(self):
+	def __init__(self,api_key):
 		self.domains = {}
+		self.api_key = api_key
+
+
+	def _search_samples(self,options):
+		'''Override search_samples in pythreatgrid (direct GET request seems a bit faster, as opposed to generator yields)
+		Args:
+			options (dict): Options for the API request.
+		Returns:
+			 json_resp (dict): JSON object for API return 
+		'''
+
+		_HOST = 'https://panacea.threatgrid.com'
+		'''str: Represents the host that the API will connect to.
+		'''
+		_APIROOT = '/api/v2'
+		'''str: The root URI for the API.
+		'''
+		_URL = _HOST + _APIROOT
+		'''str: Controls where requests will be sent.
+		'''
+		r = requests.get('%s/samples/search' % (_URL),
+		data=options)
+		json_resp = json.loads(r.text)
+
+		return json_resp 
 
 	def _get_whitelist(self):
 		try:	
@@ -29,7 +57,28 @@ class WhiteWalker():
 				pair = line.rstrip().split(',')
 				self.domains[pair[1]] = pair[0]
 			
-			
+
+	def _get_tg_artifacts(self,checksum):
+		sample_list = set()
+		resp = self._search_samples({'api_key':self.api_key,'checksum':checksum})
+		if 'data' in resp:
+			for item in resp[u'data'][u'items']:
+				sample_list.add(item['sample'])
+
+		
+		if len(sample_list) > 0:
+			urls = set()
+			for sample in sample_list:
+				resp = ''
+				for block in get_analysis({'api_key':self.api_key},sample):
+					resp += block
+
+				json_resp = json.loads(resp)	
+				for network in json_resp[u'annotations'][u'network']:
+					if 'reverse_dns' in json_resp[u'annotations'][u'network'][network]:
+						url = self._get_tld(json_resp[u'annotations'][u'network'][network][u'reverse_dns'][0].lower())
+						urls.add(url)
+			return urls
 
 	def _get_tld(self,domain):
 		dot_indexes = [x for x,y in enumerate(domain) if y == '.']
@@ -40,6 +89,7 @@ class WhiteWalker():
 		
 		return domain
 	
+
 	def _get_host_by_addr(self,ip):
 		try:
 			host = socket.gethostbyaddr(ip)[0]
@@ -74,6 +124,25 @@ class WhiteWalker():
 			print("\nCould not find %s in whitelist...\n" % (domain))
 			return False
 
+	def check_whitelist_by_hash(self,checksum):
+	
+		if len(self.domains) == 0:
+			self._get_whitelist()
+
+		tg_domains = self._get_tg_artifacts(checksum)
+		size = len(tg_domains)
+		count = 0
+		for domain in tg_domains:
+			if domain in self.domains:
+				count += 1
+				
+		if count > 0:
+			print("\nWhitelist Membership Confirmed for %s out of %s total identified domains\n" % (count,size))
+			return True
+		else:
+			print("\nCould not find any domains related to the submitted hash\n")
+			return False
+		
 	def check_whitelist_by_ip(self,ip):	
 		if len(self.domains) ==  0:
 			self._get_whitelist()
